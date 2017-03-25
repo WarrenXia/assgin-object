554b73a0ad72064bd807a8c3abcf29754c8afc3d
neo <xsh613@gmail.com>
2017-03-25 18:30:09 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var header = require('gulp-header');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var moment = require('moment');
+var pkg = require('./package.json');
+
+let timestamp = Date.now();
+let time = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+
+var headerStr = '/* author: ' + pkg.author + ' | license: ' + pkg.license + ' | github: ' + pkg.repository.url + ' | Date: ' + time + ' */\n';
+
+gulp.task('src', function() {
+  gulp.src('./assgin-object.js').pipe(header(headerStr)).pipe(gulp.dest('./src/'));
+});
+
+gulp.task('min', function() {
+  gulp.src('./assgin-object.js').pipe(uglify()).pipe(header(headerStr)).pipe(rename({suffix:'.min'})).pipe(gulp.dest('./src/'));
+});
+
+gulp.task('default', [
+  'src', 'min'
+], function() {
+  console.log('build success!');
+});
