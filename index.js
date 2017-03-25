6400c3d66f13bc53c40b732243ecd25b7d65779f
neo <xsh613@gmail.com>
2017-03-25 14:48:48 +0800

    add native object method

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function objectAssgin() {
-  let newObj = {};
-  for (let i = 0; i < arguments.length; i++) {
+function assginObject() {
+
+  if (Object.assign) {
+    return Object.assign(arguments);
+  }
+
+  var newObj = {};
+  for (var i = 0; i < arguments.length; i++) {
     if (Object.prototype.toString.call(arguments[i]).slice(8, -1) === 'Object') {
       for (j in arguments[i]) {
         newObj[j] = arguments[i][j];
       }
+    } else {
+      console.warn(arguments[i] + ' is not a object');
     }
   }
   return newObj;
 }
+
+module.exports = assginObject;
