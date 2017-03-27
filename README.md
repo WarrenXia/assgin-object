5eafa593ab9a55df589309a51dd5a9a3acda5d37
neo <xsh613@gmail.com>
2017-03-27 11:27:07 +0800

    update usage

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# assgin-object
-assgin javascript objects
+# assign-object
+
+assign javascript objects
+
+## Use the built-in
+
+Node.js 4 and up, as well as every evergreen browser (Chrome, Edge, Firefox, Opera, Safari), support `Object.assign()` :tada:. If you target only those environments, then by all means, use `Object.assign()` instead of this package.
+
+## Install
+
+```
+$ npm install --save assign-object
+```
+
+## Useage
+
+### In node environment
+```javascript
+var assignObject = require('assign-object');
+
+assignObject({1:2}, {2:5});
+//=>{1:2, 2:5}
+
+// overwrites equal keys
+assignObject({1:2}, {2:5}, {2:4});
+//=>{1:2, 2:4}
+
+// warn with null and undefined
+assignObject({1:2}, {2:5}, null);
+//=>warn:3th argument is not a pure object
+//=>{1:2, 2:5}
+
+```
+### In browser
+
+You can use in the browser, the src folder contains a compressed and uncompressed version below
---
+++ src/assign-object.js
@@ -0,0 +1,38 @@
+/* author: Neo Xia <xsh613@gmail.com> | license: MIT | github: git@github.com:WarrenXia/assign-object.git | Date: 2017-03-27 11:26:33 */
+(function() {
+  function assignObject() {
+
+    if (Object.assign) {
+      return Object.assign.apply(this, arguments);
+    }
+
+    var newObj = {};
+    var argLen = arguments.length;
+
+    if (argLen === 0) {
+      console.warn('no object to assign');
+      return null;
+    }
+
+    for (var i = 0; i < argLen; i++) {
+      if (Object.prototype.toString.call(arguments[i]).slice(8, -1) === 'Object') {
+        for (j in arguments[i]) {
+          newObj[j] = arguments[i][j];
+        }
+      } else {
+        console.warn((i + 1) + 'th argument is not a pure object');
+      }
+    }
+    return newObj;
+  }
+
+  if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    define(function() {
+      return assignObject;
+    });
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = assignObject;
+  } else {
+    window.assignObject = assignObject;
+  }
+})();
