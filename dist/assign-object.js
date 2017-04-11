b8f1aa1a67c3ca79ec3ce3c79a5d0983cdb302e9
neo <xsh613@gmail.com>
2017-04-11 19:47:06 +0800

    no message

--- dist/assign-object.js
+++ dist/assign-object.js
@@ -1,4 +1,4 @@
-/* version: 1.0.4 | author: Neo Xia <xsh613@gmail.com> | license: MIT | github: git@github.com:WarrenXia/assign-object.git | Date: 2017-03-30 12:01:14 */
+/* version: 1.0.5 | author: Neo Xia <xsh613@gmail.com> | license: MIT | github: git@github.com:WarrenXia/assign-object.git | Date: 2017-04-11 19:46:07 */
 (function() {
   function assignObject() {
 
